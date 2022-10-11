47ea44f041c08107a12a887ae86325c69fc2e7c6
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-10-10 17:15:57 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Experience from './pages/Experience'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/experience' element={<Experience />} />
-        </Routes>
-      </Router>
+    <div>
+      <Navbar />
     </div>
   );
 }
